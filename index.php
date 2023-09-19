ec2d6d98f2d94ecea7b77e0e935f57f1992a3cba
l.trentesaux <l.trentesaux@ccc.ldr>
2023-09-19 14:32:53 +0200

    index3

--- index.php
+++ index.php
@@ -2,6 +2,5 @@
 	echo 'mon premier test sur git';
 	echo '<table border=1>';
 	echo '<tr><td>Titre</td><td>description</td></tr>';
-	echo '<tr><td>Titre 1</td><td>description du titre 1</td></tr>';
 	echo '</table>';
 ?>
